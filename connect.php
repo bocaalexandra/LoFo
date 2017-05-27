fc8d64beb3f29e5163d80eec196e3424709c288b
codrutadudau <andreeacodruta89@yahoo.com>
2017-05-27 21:27:42 +0300

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,30 +1,44 @@
 <?php
 
 
- $servername = "localhost"; 
- $username = "root"; 
+ $servername = "localhost";
+ $username = "root";
  $password = "lostnfound";
  $db ='tehnologiiweb' ;
- 
- $con = new mysqli($servername, $username, $password, $db); 
- 
-	  $fname = $_POST['fname'];
+
+ $con = mysql_connect($servername, $username, $password);
+ $select_db = mysql_select_db($db);
+
+ if(isset($_POST['fname']) && isset($_POST['lname']) && isset( $_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && isset( $_POST['phone']))
+      {
+      $fname = $_POST['fname'];
       $lname = $_POST['lname'];
       $username = $_POST['username'];
       $email = $_POST['email'];
       $password = $_POST['password'];
       $phone = $_POST['phone'];
-	  
-  $query = "INSERT INTO users (fname, lname, username, email, password, phone) VALUES ('$fname', '$lname', '$username', '$email', '$password', '$phone')";
-  if(mysqli_query($con, $query))
-  {
-	  echo 'Created.';
-  }
-  else
-  {
-    echo 'Not Created.';
-  }
+      $usercheck=$email;
+      $usercheck="SELECT * FROM users WHERE email = $usercheck";
+      $result=mysql_query($usercheck,$con);
+      $yes=count($result);
+      echo $email;
 
-  header("refresh:3; url=index.html");
-  
+      if($yes >0)
+      {
+       header("refresh:3; url=index.html");
+      }
+      else
+      {
+
+  $query = "INSERT INTO users (fname, lname, username, email, password, phone) VALUES ('$fname', '$lname', '$username', '$email', '$password', '$phone')";
+  $check= mysql_query($query, $con);
+  if($check)
+  die ('Error');
+  else
+  echo "bla bla";
+
+}
+}
+
+
  ?>
