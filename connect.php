cf36296e14d9e34200654acc778f32d412fce210
codrutadudau <andreeacodruta89@yahoo.com>
2017-06-02 17:22:18 +0300

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,44 +1,68 @@
 <?php
 
+if(isset($_POST["submit"]))
+{
+  if(!empty($_POST['fname']) && !empty($_POST['lname']) && !empty($_POST['usernameus']) && !empty($_POST['email']) && !empty($_POST['passwordus']) && !empty($_POST['phone']) && !empty($_POST['gender']))
+  {
 
- $servername = "localhost";
- $username = "root";
- $password = "lostnfound";
- $db ='tehnologiiweb' ;
+   $fname = $_POST['fname'];
+   $lname = $_POST['lname'];
+   $usernameus = $_POST['usernameus'];
+   $email = $_POST['email'];
+   $passwordus = $_POST['passwordus'];
+   $phone = $_POST['phone'];
+   $gender = $_POST['gender'];
 
- $con = mysql_connect($servername, $username, $password);
- $select_db = mysql_select_db($db);
 
- if(isset($_POST['fname']) && isset($_POST['lname']) && isset( $_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && isset( $_POST['phone']))
+
+   $servername = "localhost";
+   $username = "root";
+   $password = "lostnfound";
+   $db ='tehnologiiweb' ;
+
+    $con = new mysqli($servername, $username, $password) or die (mysqli_error());
+    $select_db = mysqli_select_db($con,$db) or die ("Connot connect to the database");
+
+    $db = mysqli_query($con, "SELECT * FROM users WHERE email='".$email."' OR usernameus='".$usernameus."'");
+    $num_rows = mysqli_num_rows($db);
+
+    if($num_rows == 0)
       {
-      $fname = $_POST['fname'];
-      $lname = $_POST['lname'];
-      $username = $_POST['username'];
-      $email = $_POST['email'];
-      $password = $_POST['password'];
-      $phone = $_POST['phone'];
-      $usercheck=$email;
-      $usercheck="SELECT * FROM users WHERE email = $usercheck";
-      $result=mysql_query($usercheck,$con);
-      $yes=count($result);
-      echo $email;
-
-      if($yes >0)
+       $sql = "INSERT INTO users (fname, lname, usernameus, email, passwordus, phone, gender) VALUES ('$fname', '$lname', '$usernameus', '$email', '$passwordus', '$phone', '$gender')";
+       $result = mysqli_query($con, $sql);
+       if($result)
       {
-       header("refresh:3; url=index.html");
+        ?>
+           <!--Javascript Alert -->
+           <script>alert('Your account has been created successfully!');</script>
+        <?php
+
+        header("refresh:3; url=index_logout.html");
       }
-      else
+         else
+         {
+           ?>
+             <!--Javascript Alert -->
+             <script>alert('Failure! Please try again!');</script>
+           <?php
+         }
+      }
+     else
       {
+       ?>
+           <!--Javascript Alert -->
+           <script>alert('This E-mail or Username already exists! Please try again');</script>
+        <?php
+      }
+      }
+     else
+     {
+     ?>
+     <!--Javascript Alert -->
+     <script>alert('Required all fields');</script>
+     <?php
+     }
+     }
 
-  $query = "INSERT INTO users (fname, lname, username, email, password, phone) VALUES ('$fname', '$lname', '$username', '$email', '$password', '$phone')";
-  $check= mysql_query($query, $con);
-  if($check)
-  die ('Error');
-  else
-  echo "bla bla";
 
-}
-}
-
-
- ?>
+?>
---
+++ tehnologiiweb.sql
@@ -0,0 +1,127 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 02 Iun 2017 la 16:20
+-- Versiune server: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tehnologiiweb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structura de tabel pentru tabelul `chat_private`
+--
+
+CREATE TABLE `chat_private` (
+  `chat_date` int(10) DEFAULT NULL,
+  `chat_psw` varchar(20) NOT NULL DEFAULT '',
+  `chat_msg` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structura de tabel pentru tabelul `found`
+--
+
+CREATE TABLE `found` (
+  `id` int(11) NOT NULL,
+  `nume` varchar(30) NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `date` date NOT NULL,
+  `specific_description` varchar(1000) NOT NULL,
+  `dd1` varchar(50) NOT NULL,
+  `dd2` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `filename` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structura de tabel pentru tabelul `lost`
+--
+
+CREATE TABLE `lost` (
+  `id` int(11) NOT NULL,
+  `nume` varchar(30) NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `date` date NOT NULL,
+  `specific_description` varchar(1000) NOT NULL,
+  `dd1` varchar(50) NOT NULL,
+  `dd2` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `filename` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structura de tabel pentru tabelul `users`
+--
+
+CREATE TABLE `users` (
+  `fname` varchar(30) NOT NULL,
+  `lname` varchar(30) NOT NULL,
+  `usernameus` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `passwordus` varchar(30) NOT NULL,
+  `phone` int(10) NOT NULL,
+  `gender` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Salvarea datelor din tabel `users`
+--
+
+INSERT INTO `users` (`fname`, `lname`, `usernameus`, `email`, `passwordus`, `phone`, `gender`) VALUES
+('Codruta', 'Dudau', 'codrutadudau', 'andreeacodruta89@yahoo.com', 'lostnfound', 78956748, ''),
+('Raluca', 'Murarasu', 'ralucamurarasu', 'ralucamurarasu@yahoo.com', 'caine', 789056784, 'female');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `found`
+--
+ALTER TABLE `found`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lost`
+--
+ALTER TABLE `lost`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `found`
+--
+ALTER TABLE `found`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `lost`
+--
+ALTER TABLE `lost`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
