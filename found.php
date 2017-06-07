fce58bfbb20e85ca5809dc69508ddf4e4d538a88
stefanamurarasu <murarasu.stefana@gmail.com>
2017-06-07 18:08:56 +0300

    Add files via upload
    added functionality for uploading pictures

--- found.php
+++ found.php
@@ -1,43 +1,46 @@
 <?php
+    session_start();
+    if( !isset($_SESSION['sess_user']) ){
+      header("location:login.html");
+      exit();
+    }
 
-
-    $servername = "localhost";
-    $username = "root";
-    $password = "lostnfound";
-    $db ='tehnologiiweb' ;
-
-    $con = new mysqli($servername, $username, $password, $db);
+    $usernameus = $_SESSION['sess_user'];  
+    $con =  mysqli_connect("localhost","root","","tehnologiiweb");
 
 
    	  $nume = $_POST['nume'];
       $address = $_POST['address'];
       $date = $_POST['date'];
       $specific_description = $_POST['specific_description'];
-   	  $dd1 = $_POST['dd1'];
-   	  $dd2 = $_POST['dd2'];
-   	  $filename = $_FILES['filename']['tmp_name'];
+   	  $dd1 = $_POST['dd1']; /* dd1 for category */
+   	  $dd2 = $_POST['dd2']; /*dd2 for city*/
+   	  
+    $resultid = mysqli_fetch_assoc(mysqli_query($con,"SELECT id from users WHERE usernameus = '".$usernameus."'"));
+    $userid = $resultid['id'];
 
-     $query = "INSERT INTO found (nume, address, date, specific_description, dd1, dd2) VALUES ('$nume', '$address', '$date', '$specific_description', '$dd1', '$dd2')";
+    $result = mysqli_query($con, "INSERT INTO found (userid, nume, address, date, specific_description, dd1, dd2) VALUES ('$userid', '$nume', '$address', '$date', '$specific_description', '$dd1', '$dd2')");
 
-     if(!isset($filename))
-        echo "Please select an image.";
-     else
-     {
-          $filename = addslashes(file_get_contents($_FILES['filename']['tmp_name']));
-          $image_name = addslashes($_FILES['filename']['name']);
-          $image_name = getimagesize($_FILES['filename']['tmp_name']);
-     }
+   //if submit button has been clicked
+    if(isset($_POST['submit'])){
+        move_uploaded_file($_FILES['filename']['tmp_name'],"found-pictures/".$_FILES['filename']['name']);
 
-     if(mysqli_query($con, $query))
-     {
-   	  echo 'The announcement has been added.';
-     }
-     else
-     {
-       echo 'The announcement hasn't been added';
-     }
+        //update the picture with the one which has been added within the announcement
+       $q = mysqli_query($con,"UPDATE found SET images = '".$_FILES['filename']['name']."' WHERE userid = '".$userid."'");
 
-      header("refresh:3; url=announcements.html");
+    }
+
+
+  if($result) {
+      echo 'The announcement has been added.';
+  }
+  else {
+    echo 'The announcement hasn\'t been added.';
+  }
+
+  /*
+        de scos din comentariu. ma trimitea de pe lost, pe anunturi
+  header("refresh:3; url=announcements.html"); */
 
 
 ?>
--- lost.php
+++ lost.php
@@ -1,43 +1,44 @@
 <?php
 
+  session_start();
+  if( !isset($_SESSION['sess_user']) ){
+      header("location:login.html");
+      exit();
+  }
+  $usernameus = $_SESSION['sess_user'];  
+  $con =  mysqli_connect("localhost","root","","tehnologiiweb");
 
- $servername = "localhost";
- $username = "root";
- $password = "lostnfound";
- $db ='tehnologiiweb' ;
+  $nume = $_POST['nume'];
+  $address = $_POST['address'];
+  $date = $_POST['date'];
+  $specific_description = $_POST['specific_description'];
+  $dd1 = $_POST['dd1']; /* dd1 for category */
+  $dd2 = $_POST['dd2']; /*dd2 for city*/
 
- $con = new mysqli($servername, $username, $password, $db);
+  $resultid = mysqli_fetch_assoc(mysqli_query($con,"SELECT id from users WHERE usernameus = '".$usernameus."'"));
+  $userid = $resultid['id'];
 
+  $result = mysqli_query($con, "INSERT INTO lost (userid, nume, address, date, specific_description, dd1, dd2) VALUES ('$userid', '$nume', '$address', '$date', '$specific_description', '$dd1', '$dd2')");
 
-	  $nume = $_POST['nume'];
-      $address = $_POST['address'];
-      $date = $_POST['date'];
-      $specific_description = $_POST['specific_description'];
-	  $dd1 = $_POST['dd1'];
-	  $dd2 = $_POST['dd2'];
-	  $filename = $_FILES['filename']['tmp_name'];
+  //if submit button has been clicked
+    if(isset($_POST['submit'])){
+        move_uploaded_file($_FILES['filename']['tmp_name'],"lost-pictures/".$_FILES['filename']['name']);
 
-  $query = "INSERT INTO lost (nume, address, date, specific_description, dd1, dd2) VALUES ('$nume', '$address', '$date', '$specific_description', '$dd1', '$dd2')";
+        //update the picture with the one which has been added within the announcement
+       $q = mysqli_query($con,"UPDATE lost SET images = '".$_FILES['filename']['name']."' WHERE userid = '".$userid."'");
 
-  if(!isset($filename))
-     echo "Please select an image.";
-  else
-  {
-       $filename = addslashes(file_get_contents($_FILES['filename']['tmp_name']));
-       $image_name = addslashes($_FILES['filename']['name']);
-       $image_name = getimagesize($_FILES['filename']['tmp_name']);
+    }
+
+  if($result) {
+      echo 'The announcement has been added.';
+  }
+  else {
+    echo 'The announcement hasn\'t been added.';
   }
 
-  if(mysqli_query($con, $query))
-  {
-	  echo 'The announcement has been added.';
-  }
-  else
-  {
-    echo 'The announcement hasn't been added.';
-  }
-
-   header("refresh:3; url=announcements.html");
+  /*
+        de scos din comentariu. ma trimitea de pe lost, pe anunturi
+  header("refresh:3; url=announcements.html"); */
 
 
 
