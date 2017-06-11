bebe8d7980ffb142805990f876a8cf6bbf438741
Alexandra <boca_alexandra96@yahoo.com>
2017-06-12 00:32:42 +0300

    Add files via upload

--- ex.php
+++ ex.php
@@ -1,7 +1,9 @@
 <?php 
-if(isset($_POST['submit']))
-   {
-    $to = $_POST['email_to']; 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db ='tehnologiiweb' ;
+	$to = $_POST['email_to']; 
     $from = $_POST['email']; 
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
@@ -11,10 +13,37 @@ if(isset($_POST['submit']))
     $message2 = "Here is a copy of your email: " . $first_name . "\n\n" . $_POST['message'];
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
+	
+	$con = new mysqli($servername, $username, $password) or die (mysqli_error());
+    $select_db = mysqli_select_db($con,$db) or die ("Connot connect to the database");
+
+    $db = mysqli_query($con, "SELECT * FROM users WHERE email='".$to."'");
+    $num_rows = mysqli_num_rows($db);
+
+    if($num_rows != 0)
+      {
+       while($row = mysqli_fetch_assoc($db))
+       {
+       $db_email=$row['email'];
+       }
+	   if($to == $db_email )
+       {
+       session_start();
+       $_SESSION['sess_user']=$to;
+           
+        if(isset($_POST['submit']))
+   {
+    
+	if (filter_var($to, FILTER_VALIDATE_EMAIL)) {
     mail($to,$subject,$message,$headers);
+	echo "Mail Sent. Thank you " . $last_name . "!";
+	} else {
+    echo("$to is not a valid email address !");
+	}
     mail($from,$subject2,$message2,$headers2);
-    echo "Mail Sent. Thank you " . $last_name . "!";
-    }
+       }
+      } 
+   } else {echo "The email is not in the database !";}
 ?>
 </br></br>
 <a href="index.html">Go back</a>
