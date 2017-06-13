b91aeeb61c2684586ea814bcd2b62a858c46c6ff
stefanamurarasu <murarasu.stefana@gmail.com>
2017-06-13 11:18:19 +0300

    Create new_found.php

--- new_found.php
+++ new_found.php
@@ -47,7 +47,7 @@
 	<header>
 		<div class="container clearfix">
 			<div class="logo-wrap pull-left">
-				<a href="#" class="logo">LoFo</a>
+				<a href="#" class="logo"  onclick="location.href='index_logoutt.php'">LoFo</a>
 			</div>
 			<nav class="pull-right">
 				<ul>
