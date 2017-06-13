b3abc6608d2e8dcd63bf32bb4a564019b6819066
codrutadudau <andreeacodruta89@yahoo.com>
2017-06-13 03:38:39 +0300

    Add files via upload

--- profile.php
+++ profile.php
@@ -11,7 +11,26 @@
 	    exit();
 	}
 	$usernameus = $_SESSION['sess_user'];
-	$con =  mysqli_connect("localhost","root","","tehnologiiweb");
+	
+    $servername = "localhost";
+	$username = "root";
+    $password = "lostnfound";
+    $db ='tehnologiiweb';
+	
+	$con=mysqli_connect("localhost","root","lostnfound","tehnologiiweb");
+	
+	$userid = mysqli_fetch_assoc(mysqli_query($con, "SELECT id FROM users WHERE usernameus= '".$usernameus."'"));
+
+	try {
+    	$pdo = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+    	// set the PDO error mode to exception
+    	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    	//echo "Connected successfully"; 
+    }
+	catch(PDOException $e){
+    	echo "Connection failed: " . $e->getMessage();
+    }
+
 
 	//if submit button has been clicked
     if(isset($_POST['submit'])){
@@ -40,7 +59,7 @@
 
 <body>
 			
-	<div class="section">
+	<div class="left-section">
 		<h3>My Profile</h3>
 
 		<form action="" method="post" enctype="multipart/form-data">
@@ -51,26 +70,172 @@
 
 		<form class="myInfo">
 				
-				<label>First Name:</label>
-				<input type="text" value = "<?php echo $fname ?>" readonly/>
+				<h4>First Name:  <?php echo $fname ?></h4> 
 				<br/>
 			
-				<label>Last Name:</label>
-				<input type="text" value="<?php echo $lname ?>" readonly />
+				<h4>Last Name: <?php echo $lname ?></h4>
 				<br/>
 			
-				<label>Username:</label>
-				<input type="text" value="<?php echo $usernameus ?>" readonly/>
+				<h4>Username: <?php echo $usernameus ?></h4>
 				<br/>
 			
-				<label>E-mail address:</label>
-				<input type="text" value="<?php echo $email ?>" readonly> 
+				<h4>E-mail address: <?php echo $email ?></h4>
 				<br/>
 		</form>
 	</div>
+	<div class="right-section">
+			<?php
+				$sql = $pdo->query("SELECT * FROM lost WHERE userid= '".$userid['id']."'")->fetchall(PDO::FETCH_ASSOC);
+
+				foreach($sql as $row): ?>
+				<div class='announcement-item'>
+					<div class='announcement-type announcement-type-lost'><h4>Lost</h4>
+					</div>
+					<div class='announcement-image' style="background-image: url('lost-pictures/<?php echo $row['images'] ?>')">
+					</div>
+					<h3><?php echo $row['nume'];?></h3>
+					<div class="announcement-actions">
+						
+						<!-- The Details Modal -->
+						<input type="button" name="details" value="Details" class="btn btn-details" id="b1"/>
+						<div id="myModal" class="modal">
+								<div class="modal-content">
+										<span class="close">&times;</span>
+										<form action="" method="post" enctype="multipart/form-data">
+											<h4>Name: <?php echo $row['nume'];?> </h4></br>
+											<h4>Category: <?php echo $row['dd1'];?>  </h4></br>
+											<h4>City: <?php echo $row['dd2'];?></h4></br>
+											<h4>Address: <?php echo $row['address'];?></h4></br>
+											<h4>Date: <?php echo $row['date'];?> </h4></br>
+											<h4>More details: <?php echo $row['specific_description'];?></h4></br>
+										</form>
+								</div>
+						</div>
+						<!-- /The Details Modal -->
+						
+						<!-- The Contact Modal -->
+						<input type="button" name="contact" value="Contact" class="btn btn-contact" id="b2">
+						<div id="myModal1" class="modal">
+							<div class="modal-content">
+							<span class="close">&times;</span>
+				    		<form action="ex.php" method="post">
+								<h5>First Name: </h5><input type="text" name="first_name"></br>
+								<h5>Last Name: </h5><input type="text" name="last_name"></br>
+								<h5>From: </h5><input type="text" name="email"></br>
+								<h5>To: </h5><input type="text" name="email_to"></br>
+								<h5>Message: </h5>
+								</br>
+								<textarea id="txtArea" rows="20" name="message" cols="30">
+								</textarea><br>
+								<input type="submit" name="submit" value="Send">
+							</form>
+							</div>
+						</div>
+
+						<!-- /The Contact Modal -->
+						
+						<!-- The Report Modal -->
+						<input type="button" name="report" value="Report" class="btn btn-report" id="b3">
+						<div id="myModal2" class="modal">
+							<div class="modal-content">
+								<span class="close">&times;</span>
+								<form action="report.php" method="post">
+									<h4>Please tell us why do you want to report this announce: </h4></br>
+									<input type="radio" name="group1" value="inp"> I think the content is inapropriate</br>
+									<input type="radio" name="group1" value="inp2"> I don't think this should be on this site</br>
+									<input type="radio" name="group1" value="inp3"> This object was stoled</br>
+									<input type="radio" name="group1" value="inp4"> This announce is more than once on this website</br>
+									<input type="radio" name="group1" value="inp5"> Other reason (Please specify down below)</br>
+							        </br><textarea placeholder="Why do you want to report this announce?" id="txtArea2" rows="20" name="message2" cols="30"></textarea><br>
+									<input type="submit" name="submit2" value="Submit report">
+								</form>
+							</div>
+						</div>
+						<!-- /The Report Modal -->
+					</div>
+					
+				</div>
+				<?php
+    			endforeach;
+			?>
+		<?php
+				$sql = $pdo->query("SELECT * FROM found WHERE userid= '".$userid['id']."'")->fetchall(PDO::FETCH_ASSOC);
+
+				foreach($sql as $row): ?>
+				<div class='announcement-item'>
+					<div class='announcement-type announcement-type-found'><h4>Found</h4>
+					</div>
+					<div class='announcement-image' style="background-image: url('found-pictures/<?php echo $row['images'] ?>')">
+					</div>
+					<h3><?php echo $row['nume'];?></h3>
+					<div class="announcement-actions">
+						
+						<!-- The Details Modal -->
+						<input type="button" name="details" value="Details" class="btn btn-details" id="b1"/>
+						<div id="myModal" class="modal">
+								<div class="modal-content">
+										<span class="close">&times;</span>
+										<form action="" method="post" enctype="multipart/form-data">
+											<h4>Name: <?php echo $row['nume'];?> </h4></br>
+											<h4>Category: <?php echo $row['dd1'];?>  </h4></br>
+											<h4>City: <?php echo $row['dd2'];?></h4></br>
+											<h4>Address: <?php echo $row['address'];?></h4></br>
+											<h4>Date: <?php echo $row['date'];?> </h4></br>
+											<h4>More details: <?php echo $row['specific_description'];?></h4></br>
+										</form>
+								</div>
+						</div>
+						<!-- /The Details Modal -->
+						
+						<!-- The Contact Modal -->
+						<input type="button" name="contact" value="Contact" class="btn btn-contact" id="b2">
+						<div id="myModal1" class="modal">
+							<div class="modal-content">
+							<span class="close">&times;</span>
+				    		<form action="ex.php" method="post">
+								<h5>First Name: </h5><input type="text" name="first_name"></br>
+								<h5>Last Name: </h5><input type="text" name="last_name"></br>
+								<h5>From: </h5><input type="text" name="email"></br>
+								<h5>To: </h5><input type="text" name="email_to"></br>
+								<h5>Message: </h5>
+								</br>
+								<textarea id="txtArea" rows="20" name="message" cols="30">
+								</textarea><br>
+								<input type="submit" name="submit" value="Send">
+							</form>
+							</div>
+						</div>
+
+						<!-- /The Contact Modal -->
+						
+						<!-- The Report Modal -->
+						<input type="button" name="report" value="Report" class="btn btn-report" id="b3">
+						<div id="myModal2" class="modal">
+							<div class="modal-content">
+								<span class="close">&times;</span>
+								<form action="report.php" method="post">
+									<h4>Please tell us why do you want to report this announce: </h4></br>
+									<input type="radio" name="group1" value="inp"> I think the content is inapropriate</br>
+									<input type="radio" name="group1" value="inp2"> I don't think this should be on this site</br>
+									<input type="radio" name="group1" value="inp3"> This object was stoled</br>
+									<input type="radio" name="group1" value="inp4"> This announce is more than once on this website</br>
+									<input type="radio" name="group1" value="inp5"> Other reason (Please specify down below)</br>
+							        </br><textarea placeholder="Why do you want to report this announce?" id="txtArea2" rows="20" name="message2" cols="30"></textarea><br>
+									<input type="submit" name="submit2" value="Submit report">
+								</form>
+							</div>
+						</div>
+						<!-- /The Report Modal -->
+					</div>
+					
+				</div>
+				<?php
+    			endforeach;
+			?>
+	</div>
 	<?php
 		$usernameus = $_SESSION['sess_user'];
-        $con =  mysqli_connect("localhost","root","","tehnologiiweb");
+        $con =  mysqli_connect("localhost","root","lostnfound","tehnologiiweb");
         $query = mysqli_query($con,"SELECT images FROM users where usernameus = '".$usernameus."'");
 
         //while fetching rows from the database
